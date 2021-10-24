93031e919ce8160cba0ce9d9ae6cd0eb1230e14d
Viktoriia Fokina <viikkii.f@gmail.com>
2021-10-25 00:31:46 +0300

    ДЗ-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+console.log(galleryEl);
+
+const cartMarkup = galleryItems
+    .map((preview, original, description) => {
+   return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}."
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+    
+galleryEl.insertAdjacentHTML("beforeend", cartMarkup);
+console.log(cartMarkup);
